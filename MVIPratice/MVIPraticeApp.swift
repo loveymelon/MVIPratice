f9c24ca0dbf387a244909bfebe4f8e42fb54902e
jin <manciepmc@gmail.com>
2024-06-07 18:31:50 +0900

    Initial Commit

---
+++ MVIPratice/MVIPraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVIPraticeApp.swift
+//  MVIPratice
+//
+//  Created by 김진수 on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MVIPraticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
